836ccf8ceda67309e8f283f6b7c02077d8d94082
Eugene Idan <eugeneidan@gmail.com>
2015-10-30 12:49:08 +0000

    add users and finish styles

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -6,6 +6,7 @@ class Recipe < ActiveRecord::Base
 	#Associations
 	has_many :ingredients
 	has_many :directions
+	belongs_to :user
 
 	accepts_nested_attributes_for :ingredients,
 									reject_if: proc { |attributes| attributes['name'].blank? },
